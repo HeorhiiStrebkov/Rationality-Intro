fb2cbc8f082a67e62e8659f67b985b4252b44e7c
HeorhiiStrebkov <georgstrebkov@yandex.ru>
2019-04-20 12:32:28 +0300

    db project

---
+++ RationalityIntro.SQL/dbo/Tables/Act.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Act] (
+    [Id]          INT            NOT NULL,
+    [Description] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Act] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ RationalityIntro.SQL/dbo/Tables/Human.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Human] (
+    [Id]              INT            NOT NULL,
+    [Sex]             NVARCHAR (MAX) NULL,
+    [Profession]      NVARCHAR (MAX) NULL,
+    [SubProperty]     NVARCHAR (MAX) NULL,
+    [RelationsWithId] INT            NULL,
+    CONSTRAINT [PK_Human] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Human_Human1] FOREIGN KEY ([RelationsWithId]) REFERENCES [dbo].[Human] ([Id])
+);
+
