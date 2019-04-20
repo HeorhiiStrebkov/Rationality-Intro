a759f5086966117382a651ec32cdd162be8f06ad
HeorhiiStrebkov <georgstrebkov@yandex.ru>
2019-04-20 12:07:53 +0300

    Structure commit

---
+++ RationalityIntro.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RationalityIntro.Web.MvcApplication" Language="C#" %>
---
+++ RationlityIntro.API/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace RationlityIntro.API.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
